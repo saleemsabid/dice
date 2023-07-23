3d7bfb0d0d495b11efe8a708a2006fc34a87b5a3
saleemsabid <saleemsabid@gmail.com>
2023-07-23 12:30:35 +0530

    made hello world

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,47 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: GradientContainer(),
+    );
+  }
+}
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+  @override
+  Widget build(context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 123, 108, 156),
+              Color.fromARGB(255, 89, 60, 156),
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: const Center(
+          child: Text(
+            'Hello World',
+            style: TextStyle(color: Colors.white, fontSize: 30.0, shadows: [
+              Shadow(
+                  color: Color.fromARGB(255, 37, 37, 37),
+                  blurRadius: 5,
+                  offset: Offset.zero)
+            ]),
+          ),
+        ),
+      ),
+    );
   }
 }

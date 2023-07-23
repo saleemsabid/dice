f4f7fb90c4c5fb41ad15d1c02b9c5ccccae8f3a5
saleemsabid <saleemsabid@gmail.com>
2023-07-23 12:35:00 +0530

    split code into different files

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:dice/gradient_container.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -14,38 +16,6 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: GradientContainer(),
-    );
-  }
-}
-
-class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
-  @override
-  Widget build(context) {
-    return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Color.fromARGB(255, 123, 108, 156),
-              Color.fromARGB(255, 89, 60, 156),
-            ],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-          ),
-        ),
-        child: const Center(
-          child: Text(
-            'Hello World',
-            style: TextStyle(color: Colors.white, fontSize: 30.0, shadows: [
-              Shadow(
-                  color: Color.fromARGB(255, 37, 37, 37),
-                  blurRadius: 5,
-                  offset: Offset.zero)
-            ]),
-          ),
-        ),
-      ),
     );
   }
 }
